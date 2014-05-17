9beda0a7ab8b623d13d5868f4980cc633f925864
Zac Staples <zacstaples@mac.com>
2014-05-16 19:23:45 -0700

    modified control inputs
    updated control inputs to reflect correct pin numbers for rt/lt
    and the center value for stop

--- williams/williams.ino
+++ williams/williams.ino
@@ -2,24 +2,33 @@
 
 #define DEBUG_SERVO 1  //set to 1 for debug print to serial monitor
 
-//from servo conf our right servo is centered at 92
-//from servo conf our left servo is centered at 88
+//from servo conf our right servo is centered at 91
+//from servo conf our left servo is centered at 87
 
 Servo servo_right, servo_left;
 
 const int servo_right_pin = 10;
 const int servo_left_pin = 9;
 
-char incoming_byte = 0;
-const char fwd_left = '7';
-const char fwd_straight = '8';
-const char fwd_right = '9';
-const char spin_left = '4';
-const char spin_right = '6';
-const char stop_all = '5';
-const char back_left = '1';
+const int servo_right_offset = 1;
+const int servo_left_offset = -3;
+const int r_mult = 10;
+const int l_mult = -10;
+const int servo_center = 90;
+
+int r_val;
+int l_val;
+
+char incoming_byte =       '0';
+const char fwd_left =      '7';
+const char fwd_straight =  '8';
+const char fwd_right =     '9';
+const char spin_left =     '4';
+const char spin_right =    '6';
+const char stop_all =      '5';
+const char back_left =     '1';
 const char back_straight = '2';
-const char back_right = '3';
+const char back_right =    '3';
 
 void setup() {
   Serial.begin(57600);
@@ -29,28 +38,48 @@ void setup() {
 
 }
 
-void loop() {
+char read_serial() {
+   //read the serial monitor and return any incoming char
+  char res = '0';
   if (Serial.available() > 0) {
-    incoming_byte = Serial.read();
+    res = Serial.read();
     if(DEBUG_SERVO) {
       Serial.print("incoming byte is: ");
       Serial.println(incoming_byte);  
     }
+  }
+  return res;  
+}
+
+void loop() {
+    
+    servo_right.write(r_val);
+    servo_left.write(l_val);
+
+    incoming_byte = read_serial();
     
     switch(incoming_byte){
       case fwd_straight:
-          servo_right.write(120);
-          servo_left.write(60);
+          r_val = 3 * r_mult + servo_center;
+          l_val = 3 * l_mult + servo_center;
         break;
       case back_straight:
-          servo_right.write(60);
-          servo_left.write(120);      
+          r_val = -3 * r_mult + servo_center;
+          l_val = -3 * l_mult + servo_center;
         break;
-      default:
- //         servo_right.write(90);
- //         servo_left.write(90);
-    ;}
-  }
+      case stop_all:
+          r_val = servo_center + servo_right_offset;
+          l_val = servo_center + servo_left_offset;
+        break;
+    }
+
+    if(DEBUG_SERVO) {
+      Serial.print("left: ");
+      Serial.print(l_val);  
+      Serial.print("/right: ");
+      Serial.println(l_val);  
+    }
+
 }
 
 
