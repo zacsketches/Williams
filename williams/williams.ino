b9fad5ece1a7e27760fd0fd15d8a18a0a0735bbc
c. Sewing <csewing@nps.edu>
2014-05-16 18:11:31 -0700

    requirement for IDE

--- williams/williams.ino
+++ williams/williams.ino
@@ -7,16 +7,16 @@ Servo servo_right, servo_left;
 const int servo_right_pin = 9;
 const int servo_left_pin = 10;
 
-int incoming_byte = 0;
-const int fwd_left = 7;
-const int fwd_straight = 8;
-const int fwd_right = 9;
-const int spin_left = 4;
-const int spin_right = 6;
-const int stop_all = 5;
-const int back_left = 1;
-const int back_straight = 2;
-const int back_right = 3;
+char incoming_byte = 0;
+const char fwd_left = 7;
+const char fwd_straight = '8';
+const char fwd_right = 9;
+const char spin_left = 4;
+const char spin_right = 6;
+const char stop_all = '5';
+const char back_left = 1;
+const char back_straight = '2';
+const char back_right = 3;
 
 void setup() {
   Serial.begin(57600);
@@ -29,7 +29,6 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     incoming_byte = Serial.read();
-    
     if(DEBUG_SERVO) {
       Serial.print("incoming byte is: ");
       Serial.println(incoming_byte);  
@@ -41,13 +40,13 @@ void loop() {
           servo_left.write(60);
         break;
       case back_straight:
-          servo_right.write(120);
-          servo_left.write(60);      
+          servo_right.write(60);
+          servo_left.write(120);      
         break;
       default:
-          servo_right.write(90);
-          servo_left.write(90);
-    }
+ //         servo_right.write(90);
+ //         servo_left.write(90);
+    ;}
   }
 }
 
