03d3b8b1eb613fed4e0a231489f79592af2533a7
Zac Staples <zacstaples@mac.com>
2014-05-18 11:16:15 -0700

    Added a test program to start working with the Esplora
    First addition to the repo looking toward building a wireless control
    for Williams from an Arduino Esplora board.

---
+++ joystick_no_xbee/joystick_no_xbee.ino
@@ -0,0 +1,27 @@
+#include <Esplora.h>
+
+/*  Test sketch to ensure the Esplora hordware is working in accordance 
+    the system description.  
+    
+    I simply want to establish Serial comms with the Arduino IDE 
+    serial monitor to see values for the joystick inputs.
+*/
+
+void setup()
+{
+  Serial.begin(9600);       // initialize serial communication with your computer
+} 
+
+void loop()
+{
+  int xValue = Esplora.readJoystickX();        // read the joystick's X position
+  int yValue = Esplora.readJoystickY();        // read the joystick's Y position
+  Serial.print("Joystick X: ");                // print a label for the X value
+  Serial.print(xValue);                        // print the X value
+  Serial.print("\tY: ");                       // print a tab character and a label for the Y value
+  Serial.println(yValue);                      // print the Y value
+  
+  delay(10);                                  // a short delay before moving again
+}
+
+
